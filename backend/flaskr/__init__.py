dd12f68fa137dabf8f60eb3cf4a08214e2a40551
libscode <lmtlibasse@gmail.com>
2022-08-20 14:02:30 +0200

    endppoint categories question search and quiz

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -16,17 +16,29 @@ def create_app(test_config=None):
     """
     @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
     """
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     """
     @TODO: Use the after_request decorator to set Access-Control-Allow
     """
-
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization, true')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
     """
     @TODO:
     Create an endpoint to handle GET requests
     for all available categories.
     """
-
+    @app.route('/categories')
+    def get_categories():
+        categories = Category.query.all()
+        categories = {category.id: category.type for category in categories}
+        return jsonify({
+            'success': True,
+            'categories': categories
+        })
 
     """
     @TODO:
@@ -40,6 +52,22 @@ def create_app(test_config=None):
     ten questions per page and pagination at the bottom of the screen for three pages.
     Clicking on the page numbers should update the questions.
     """
+    @app.route('/questions')
+    def get_questions():
+        page = request.args.get('page', 1, type=int)
+        start = (page - 1) * QUESTIONS_PER_PAGE
+        end = start + QUESTIONS_PER_PAGE
+        questions = Question.query.all()
+        current_questions = questions[start:end]
+        categories = Category.query.all()
+        categories = {category.id: category.type for category in categories}
+        return jsonify({
+            'success': True,
+            'questions': [question.format() for question in current_questions],
+            'total_questions': len(questions),
+            'categories': categories,
+            'current_category': None
+        })
 
     """
     @TODO:
@@ -48,6 +76,20 @@ def create_app(test_config=None):
     TEST: When you click the trash icon next to a question, the question will be removed.
     This removal will persist in the database and when you refresh the page.
     """
+
+    @app.route('/questions/<int:question_id>', methods=['DELETE'])
+    def delete_question(question_id):
+        try:
+            question = Question.query.filter(Question.id == question_id).one_or_none()
+            if question is None:
+                abort(404)
+            question.delete()
+            return jsonify({
+                'success': True,
+                'deleted': question_id
+            })
+        except:
+            abort(422)
 
     """
     @TODO:
@@ -59,6 +101,22 @@ def create_app(test_config=None):
     the form will clear and the question will appear at the end of the last page
     of the questions list in the "List" tab.
     """
+    @app.route('/questions', methods=['POST'])
+    def create_question():
+        body = request.get_json()
+        question = body.get('question', None)
+        answer = body.get('answer', None)
+        category = body.get('category', None)
+        difficulty = body.get('difficulty', None)
+        try:
+            question = Question(question=question, answer=answer, category=category, difficulty=difficulty)
+            question.insert()
+            return jsonify({
+                'success': True,
+                'created': question.id
+            })
+        except:
+            abort(422)
 
     """
     @TODO:
@@ -70,6 +128,17 @@ def create_app(test_config=None):
     only question that include that string within their question.
     Try using the word "title" to start.
     """
+    @app.route('/search', methods=['POST'])
+    def search_questions():
+        body = request.get_json()
+        search_term = body.get('searchTerm', None)
+        questions = Question.query.filter(Question.question.ilike(f'%{search_term}%')).all()
+        return jsonify({
+            'success': True,
+            'questions': [question.format() for question in questions],
+            'total_questions': len(questions),
+            'current_category': None
+        })
 
     """
     @TODO:
@@ -79,6 +148,15 @@ def create_app(test_config=None):
     categories in the left column will cause only questions of that
     category to be shown.
     """
+    @app.route('/categories/<int:category_id>/questions')
+    def get_questions_by_category(category_id):
+        questions = Question.query.filter(Question.category == category_id).all()
+        return jsonify({
+            'success': True,
+            'questions': [question.format() for question in questions],
+            'total_questions': len(questions),
+            'current_category': category_id
+        })
 
     """
     @TODO:
@@ -91,6 +169,26 @@ def create_app(test_config=None):
     one question at a time is displayed, the user is allowed to answer
     and shown whether they were correct or not.
     """
+    @app.route('/quizzes', methods=['POST'])
+    def get_quiz_questions():
+        body = request.get_json()
+        previous_questions = body.get('previous_questions', None)
+        category = body.get('quiz_category', None)
+        if category['id'] == 0:
+            questions = Question.query.filter(Question.id.notin_(previous_questions)).all()
+        else:
+            questions = Question.query.filter(Question.category == category['id']).filter(Question.id.notin_(previous_questions)).all()
+        if len(questions) == 0:
+            return jsonify({
+                'success': True,
+                'question': None
+            })
+        else:
+            question = random.choice(questions)
+            return jsonify({
+                'success': True,
+                'question': question.format()
+            })
 
     """
     @TODO:
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -12,8 +12,8 @@ class TriviaTestCase(unittest.TestCase):
 
     def setUp(self):
         """Define test variables and initialize app."""
-        self.app = create_app()
-        self.client = self.app.test_client
+        self.app           = create_app()
+        self.client        = self.app.test_client
         self.database_name = "trivia_test"
         self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
